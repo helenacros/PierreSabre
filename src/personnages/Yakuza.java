6f66119ec588c4daa064a7f310bbb3050aba39a0
helenacros <lna.cros@gmail.com>
2024-11-18 15:23:26 +0100

    fin du TP4

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -5,8 +5,13 @@ public class Yakuza extends Humain {
 	private String clan;
 	
 	
+	
 	public Yakuza(String nom,String boisson, int argent, String clan) {
 		super(nom,boisson,argent); //Seulement sa definition en tant qu'humain
+	}
+	
+	public int getReputation() {
+		return reputation;
 	}
 	
 	public void extorquer(Commercant victime) {
@@ -17,6 +22,22 @@ public class Yakuza extends Humain {
 		parler("J'ai piqué les "+argentDeMarco+" de "+victime.getNom()+" ce qui me fait "+ (getArgent()+argentDeMarco)+" sous dans ma poche. Hi ! Hi!");
 		
 	}
+	
+	public int perdre() {
+		int argent = getArgent();
+		setArgent(0);
+		reputation -=1;
+		parler("J'ai perdu mon duel et mes " + argent + " sous, snif... J'ai dÃ©shonorÃ© le clan de " + clan);
+		return argent;
+	}
+	
+	public void gagner(int gain) {
+		setArgent(getArgent() + gain);
+		reputation +=1;
+		parler("Ce Ronin pensait vraiment battre " + getNom() + " du clan de " + clan + " ? Je l'ai dÃ©pouillÃ© de ses " + gain + " sous.");
+	} 
+	
+	
 	
 
 }
